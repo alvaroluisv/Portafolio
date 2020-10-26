19b7aa0c62a9d6f313b1fde459658bbc9fd49fff
alvaroluisv <alvaroluisvasquez@gmail.com>
2020-10-26 01:29:54 -0400

    Fin de la Seccion 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,44 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { AboutComponent } from './pages/about/about.component';
+import { DocumentationComponent } from './pages/documentation/documentation.component';
+import { DesingComponent } from './pages/desing/desing.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ProductoComponent } from './pages/producto/producto.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {
+    path: 'home', component: PortafolioComponent
+  },
+  {
+    path: 'blog', component: BlogComponent
+  },
+  {
+  path: 'about', component: AboutComponent
+  },
+  {
+    path: 'documentation', component: DocumentationComponent
+  },
+  {
+    path: 'design', component: DesingComponent
+  },
+  {
+    path: 'contact', component: ContactComponent
+  },
+  {
+    path: 'producto', component: ProductoComponent
+  },
+  {
+    path: '**', pathMatch: 'full', redirectTo: 'home'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,25 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { DocumentationComponent } from './pages/documentation/documentation.component';
+import { DesingComponent } from './pages/desing/desing.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProductoComponent } from './pages/producto/producto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    BlogComponent,
+    DocumentationComponent,
+    DesingComponent,
+    ContactComponent,
+    AboutComponent,
+    ProductoComponent
   ],
   imports: [
     BrowserModule,
